0e483fc0fc979808a69e75e065a5393c88cc9f77
Ahmet Hrnjadovic <ahmet.hrnjadovic@gmail.com>
2022-07-23 20:23:51 +0200

    replace protoc-rust with tonic-build

--- rsi_service/build.rs
+++ rsi_service/build.rs
@@ -1,11 +1,7 @@
-extern crate protoc_rust;
-
-fn main() {
-    protoc_rust::Codegen::new()
-        .out_dir("src/protos")
-        .inputs(&["../api/monitor.proto"])
-        .include("../api/")
-        .protoc_path("../../protoc/bin/protoc")
-        .run()
-        .expect("Running protoc failed.");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+   tonic_build::configure()
+        .out_dir("src/protos/")
+        .compile(&["../api/monitor.proto"],
+                 &["../api/"])?;
+   Ok(())
 }
