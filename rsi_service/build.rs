c6a45c76b6e17ca5d533d53a5296f689c78ea93b
Ahmet Hrnjadovic <ahmet.hrnjadovic@gmail.com>
2022-07-26 13:24:17 +0200

    update build.rs to include rsi.proto

--- rsi_service/build.rs
+++ rsi_service/build.rs
@@ -1,7 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
    tonic_build::configure()
         .out_dir("src/protos/")
-        .compile(&["../api/monitor.proto"],
+        .compile(&["../api/monitor.proto", "../api/rsi.proto"],
                  &["../api/"])?;
    Ok(())
 }
