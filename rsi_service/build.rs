ba277e03fc5cce603a776cead6859bc3d9274db4
Ahmet Hrnjadovic <ahmet.hrnjadovic@gmail.com>
2022-07-27 22:11:08 +0200

    do not autogenerate client code from proto

--- rsi_service/build.rs
+++ rsi_service/build.rs
@@ -1,6 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
    tonic_build::configure()
-        .out_dir("src/protos/")
+        .build_client(false)
+        .out_dir("./src/protos/")
         .compile(&["../api/monitor.proto", "../api/rsi.proto"],
                  &["../api/"])?;
    Ok(())
