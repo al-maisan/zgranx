0895e5091bd1f142213c36486f9f5706b191f2c8
Muharem Hrnjadovic <m@sky1.vip>
2022-07-28 12:04:29 +0200

    add ping req to etc

--- cmd/exa/etc/main.go
+++ cmd/exa/etc/main.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/alphabot-fi/T-801/internal/proto/exa"
+	"github.com/alphabot-fi/T-801/internal/proto/monitor"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
@@ -91,13 +92,44 @@ func main() {
 					if saddr == "" {
 						saddr = defaultAddr
 					}
-					log.Info("get-balances, saddr = ", saddr)
+					log.Info("get-open-orders, saddr = ", saddr)
 					conn, err := grpc.Dial(saddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 					if err != nil {
 						log.Fatalf("did not connect: %v", err)
 					}
 					defer conn.Close()
 					getOrders(conn, exchange)
+					return nil
+				},
+			},
+			{
+				Name:  "ping",
+				Usage: "ping the exchange adapter for the given exchange",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:        "exchange",
+						Usage:       "exchange to use (e.g. \"huobi\")",
+						Required:    true,
+						Destination: &exchange,
+					},
+					&cli.StringFlag{
+						Name:        "server-address",
+						Usage:       "server address (e.g. \"localhost:50051\")",
+						Required:    false,
+						Destination: &saddr,
+					},
+				},
+				Action: func(c *cli.Context) error {
+					if saddr == "" {
+						saddr = defaultAddr
+					}
+					log.Info("ping, saddr = ", saddr)
+					conn, err := grpc.Dial(saddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+					if err != nil {
+						log.Fatalf("did not connect: %v", err)
+					}
+					defer conn.Close()
+					ping(conn, exchange)
 					return nil
 				},
 			},
@@ -174,4 +206,22 @@ func apiKeys(exchange, access string) [2]string {
 	apiKey := os.Getenv(fmt.Sprintf("%s_%s_ACCESS_KEY", strings.ToUpper(exchange), strings.ToUpper(access)))
 	secretKey := os.Getenv(fmt.Sprintf("%s_%s_SECRET_KEY", strings.ToUpper(exchange), strings.ToUpper(access)))
 	return [2]string{apiKey, secretKey}
+}
+
+func ping(c *grpc.ClientConn, exchange string) {
+	cmon := monitor.NewMonitorClient(c)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	req := monitor.PingRequest{
+		RequestTime: timestamppb.Now(),
+	}
+	log.Infof("ping for %s at %v", exchange, time.Now().UTC())
+	res, err := cmon.Ping(ctx, &req)
+	if err != nil {
+		log.Errorf("failed to get ping EXA for %s, %v", exchange, err)
+		return
+	}
+	log.Infof("ping response from %s EXA at %v, version('%s')", exchange, res.ResponseTime.AsTime().UTC(), res.Version)
 }
