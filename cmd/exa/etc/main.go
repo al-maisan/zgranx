e3d621bef28bb9be85eb18b34ec670f8f837bcde
Muharem Hrnjadovic <m@sky1.vip>
2022-07-28 11:51:47 +0200

    finish the etc side

--- cmd/exa/etc/main.go
+++ cmd/exa/etc/main.go
@@ -69,6 +69,38 @@ func main() {
 					return nil
 				},
 			},
+			{
+				Name:    "get-open-orders",
+				Aliases: []string{"goo"},
+				Usage:   "get the open orders for the given exchange",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:        "exchange",
+						Usage:       "exchange to use (e.g. \"huobi\")",
+						Required:    true,
+						Destination: &exchange,
+					},
+					&cli.StringFlag{
+						Name:        "server-address",
+						Usage:       "server address (e.g. \"localhost:50051\")",
+						Required:    false,
+						Destination: &saddr,
+					},
+				},
+				Action: func(c *cli.Context) error {
+					if saddr == "" {
+						saddr = defaultAddr
+					}
+					log.Info("get-balances, saddr = ", saddr)
+					conn, err := grpc.Dial(saddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+					if err != nil {
+						log.Fatalf("did not connect: %v", err)
+					}
+					defer conn.Close()
+					getOrders(conn, exchange)
+					return nil
+				},
+			},
 		},
 	}
 
@@ -98,13 +130,43 @@ func getBalances(c *grpc.ClientConn, exchange string) {
 		log.Errorf("failed to get balances for %s, %v", exchange, err)
 		return
 	}
-	log.Infof("got balances for %s huobi at %v", exchange, res.ResponseTime)
+	log.Infof("got balances for %s at %v", exchange, res.ResponseTime.AsTime().UTC())
 	for i, b := range res.Balances {
 		if b == nil {
 			log.Errorf("nil balance for index %d", i)
 			continue
 		}
 		fmt.Printf("[%2d] %10s %6s %20s\n", i, b.GetAccount(), b.GetAsset(), b.GetBalance())
+	}
+}
+
+func getOrders(c *grpc.ClientConn, exchange string) {
+	aks := apiKeys(exchange, "ro")
+	cexa := exa.NewEXAClient(c)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	req := exa.GetOpenOrdersRequest{
+		RequestTime: timestamppb.Now(),
+		RequestId:   fmt.Sprintf("%d", time.Now().UTC().Unix()),
+		Exchange:    "huobi",
+		ApiKey:      aks[0],
+		ApiSecret:   aks[1],
+	}
+	log.Infof("get orders for %s at %v (%s)", exchange, time.Now().UTC(), req.GetRequestId())
+	res, err := cexa.GetOpenOrders(ctx, &req)
+	if err != nil {
+		log.Errorf("failed to get orders for %s, %v", exchange, err)
+		return
+	}
+	log.Infof("got orders for %s at %v", exchange, res.ResponseTime.AsTime().UTC())
+	for i, b := range res.Orders {
+		if b == nil {
+			log.Errorf("nil order for index %d", i)
+			continue
+		}
+		fmt.Printf("[%2d] %10s %8s %6\n", i, b.AccountId, b.Symbol, b.Price)
 	}
 }
 
