fec71bf1c5226d604f9b37fe49fc9d548683305c
Ahmet Hrnjadovic <ahmet.hrnjadovic@gmail.com>
2022-08-11 15:46:21 +0200

    Revert "Feature/rsi service (#20)"
    This reverts commit 7b2114940461867ce2ef0ff54fdbe5ab0cafbcf3.

--- rsi_service/build.rs
+++ rsi_service/build.rs
@@ -1,8 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
    tonic_build::configure()
-        .build_client(false)
-        .out_dir("./src/protos/")
-        .compile(&["../api/monitor.proto", "../api/rsi.proto"],
+        .out_dir("src/protos/")
+        .compile(&["../api/monitor.proto"],
                  &["../api/"])?;
    Ok(())
 }
