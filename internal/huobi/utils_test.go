4ce79cef33ca3890140667922a686a5ced92171b
Muharem Hrnjadovic <muharem@pm.me>
2022-08-12 07:35:44 +0200

    fix broken test (#26)

--- internal/huobi/utils_test.go
+++ internal/huobi/utils_test.go
@@ -3,19 +3,19 @@ package huobi
 import (
 	"testing"
 
-	"github.com/alphabot-fi/T-801/internal/proto/exa"
+	"github.com/alphabot-fi/T-801/internal/proto/base"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestString2pair(t *testing.T) {
 	p, err := String2pair("ethusdt")
 	assert.Nil(t, err)
-	assert.Equal(t, exa.Asset_ETH, p.Base)
-	assert.Equal(t, exa.Asset_USDT, p.Quote)
+	assert.Equal(t, base.Asset_ETH, p.Base)
+	assert.Equal(t, base.Asset_USDT, p.Quote)
 	p, err = String2pair("avaxusdt")
 	assert.Nil(t, err)
-	assert.Equal(t, exa.Asset_AVAX, p.Base)
-	assert.Equal(t, exa.Asset_USDT, p.Quote)
+	assert.Equal(t, base.Asset_AVAX, p.Base)
+	assert.Equal(t, base.Asset_USDT, p.Quote)
 	p, err = String2pair("btcusd")
 	assert.Nil(t, p)
 	assert.EqualError(t, err, "unknown quote asset: 'usd'")
